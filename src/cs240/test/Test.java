4a225c27b9cef81de89d76577aa8ba5539c033d7
Ronald Lencevicius <rlencevicius@gmail.com>
2017-03-12 20:41:34 -0700

    nothing to see here

--- src/cs240/test/Test.java
+++ src/cs240/test/Test.java
@@ -1,32 +1,45 @@
 package cs240.test;
 
 import cs240.list.*;
+import cs240.queue.*;
+import cs240.stack.*;
+
+import java.util.Iterator;
+
+import cs240.dictionary.*;
 
 public class Test {
 	public static void main(String[] args) {
-		SingleLinkedList<Object> AL = new SingleLinkedList<Object>();
+		Dictionary<Object, Object> D = new Dictionary<Object, Object>();
+	
+		D.add(1, 'a');
+		D.add(1, 'b');
+		D.add(2, 'c');
+		D.add(3, 'D');
 		
-		AL.add(1);
-		AL.add(2);
-		AL.add(3);
-		AL.add(4);
-		AL.add(5);
-		System.out.print("\n{ ");
-		for(int i = 0; i < AL.getLength(); i++) {
-			System.out.print(AL.getEntry(i) + " ");
-		}System.out.print("}\n");
+		Iterator<Object> k = D.getKeyIterator();
+		Iterator<Object> v = D.getValueIterator();
 		
-		AL.add(2, "h");
-		System.out.println(AL.contains("h"));
-	//	AL.replace(2, "x");
-	//	AL.clear();
+		System.out.println(v.hasNext());
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
 		
-		System.out.print("\n{ ");
-		for(int i = 0; i < AL.getLength(); i++) {
-			System.out.print(AL.getEntry(i) + " ");
-		}System.out.print("}");
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
 		
-		System.out.println("Position 5: " + AL.getEntry(5));
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
 		
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
+		
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
+		
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
+		
+		System.out.println(v.next());
+		System.out.println(v.hasNext());
 	}
 }
