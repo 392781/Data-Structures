6e38a6de54d0c6b7eaa8d589728405fa67dff5e1
Ronald Lencevicius <rlencevicius@gmail.com>
2017-03-12 22:55:06 -0700

    Nothing to see

--- src/cs240/test/Test.java
+++ src/cs240/test/Test.java
@@ -4,42 +4,11 @@ import cs240.list.*;
 import cs240.queue.*;
 import cs240.stack.*;
 
-import java.util.Iterator;
-
 import cs240.dictionary.*;
 
 public class Test {
 	public static void main(String[] args) {
-		Dictionary<Object, Object> D = new Dictionary<Object, Object>();
-	
-		D.add(1, 'a');
-		D.add(1, 'b');
-		D.add(2, 'c');
-		D.add(3, 'D');
+		SingleLinkedList<Object> SLL = new SingleLinkedList<Object>();
 		
-		Iterator<Object> k = D.getKeyIterator();
-		Iterator<Object> v = D.getValueIterator();
-		
-		System.out.println(v.hasNext());
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
-		
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
-		
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
-		
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
-		
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
-		
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
-		
-		System.out.println(v.next());
-		System.out.println(v.hasNext());
 	}
 }
